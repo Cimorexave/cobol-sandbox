105e80484171a56efec86dd414598d40de8a6d62
Cimorexave <sadeq.s502@gmail.com>
2024-03-29 15:39:41 +0330

    init input prog

---
+++ input.cbl
@@ -0,0 +1,8 @@
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  input.
+       AUTHOR. sina sadeqi. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 01/01/08. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
