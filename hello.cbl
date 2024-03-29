72d53e0d0d5b80fc1647a209e464a0fad1a17339
Cimorexave <sadeq.s502@gmail.com>
2024-03-29 15:39:26 +0330

    init and tested hello world program

---
+++ hello.cbl
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  hello.
+       AUTHOR. Sina Sadeqi. 
+       INSTALLATION. COBOL DEVELOPMENT CENTER. 
+       DATE-WRITTEN. 29/03/2024. 
+       SECURITY. NON-CONFIDENTIAL.
+      *****************************************************************
+      ******************************************************************
+       PROCEDURE DIVISION.
+           DISPLAY  "HELLO WORLD".
+           STOP RUN.
