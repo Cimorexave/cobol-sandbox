2aeed2264876f209405936bf8189f19b0bd51790
Cimorexave <sadeq.s502@gmail.com>
2025-09-22 12:22:06 +0200

    copybooks

---
+++ calls/book.cpy
@@ -0,0 +1,3 @@
+       01  common-id pic S9(1) value 1.
+       01  common-name pic X(50) value "john pork".
+       
--- calls/caller.cbl
+++ calls/caller.cbl
@@ -5,10 +5,13 @@
        working-storage section.
        01  msg pic x(256) value "hello".
        *>> load copybook 
+       copy "./calls/book.cpy".
        
        *>> <summary>calls the callee program by passing vars</summary>
        procedure division.
            display "CALLER...".
            call "callee" using by reference msg.
-       
+           display "copybook: " common-id.
+           display "copybook: " common-name.
+       end program caller.
        
